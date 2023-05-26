b739caa3139351c59689073256cd1e988af5b582
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-26 15:01:07 +0200

    modified age

--- js/script.js
+++ js/script.js
@@ -5,5 +5,5 @@ const placeholder = document.getElementById(`costoFinale`);
 console.log(costoFinale);
 
 // chiedi al utente l'eta
-const age = prompt(`inserisci la tua età`);
+const age = prompt(`inserisci la tua età`,`15`);
 console.log(age);
