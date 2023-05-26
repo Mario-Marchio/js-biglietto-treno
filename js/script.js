35408b68113bfc72a026f8a1023ff31335cfae0c
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-26 15:02:09 +0200

    km added

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ console.log(costoFinale);
 // chiedi al utente l'eta
 const age = prompt(`inserisci la tua età`,`15`);
 console.log(age);
+
+// chiedi al utente il numero di km che vuole percorrere
+const km = prompt(`inserisci il numero di km che vuoi percorrere`,`100`);
+console.log(km);
