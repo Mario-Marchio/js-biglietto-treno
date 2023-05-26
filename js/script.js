dc6e97c9d5c12353ca8b31c50c5a446e0d313fd1
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-26 15:00:29 +0200

    age added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ console.log(`JS OK`);
 // recupero placeholder dal dom 
 const placeholder = document.getElementById(`costoFinale`);
 console.log(costoFinale);
+
+// chiedi al utente l'eta
+const age = prompt(`inserisci la tua età`);
+console.log(age);
